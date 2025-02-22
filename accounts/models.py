77cf0bfbc6451a5b14a5460fd40fd8f66d1f244e
maamirmughal <101181756+mc514@users.noreply.github.com>
2025-02-22 19:46:43 +0400

    display data in templates,status.html updated, fetch data using for loop

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Tag(models.Model):
+	name = models.CharField(max_length=20, null=True)
+	
+	def __str__(self):
+	    return self.name
+
 class Customer(models.Model):
 	name = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=18, null=True)
@@ -22,18 +28,13 @@ class Product(models.Model):
 	name = models.CharField(max_length=200, null=True) # null=True with this we get error
 	price = models.FloatField(null=True) 
 	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-	description = models.TextField()
+	description = models.TextField(max_length=500, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+	tags = models.ManyToManyField(Tag)
 	
 	def __str__(self):
 		return self.name
-	
-class Tag(models.Model):
-	name = models.CharField(max_length=20, null=True)
-	
-	def __str__(self):
-	    return self.name
-        
+     
 
 class Order(models.Model):
 	STATUS = (
@@ -46,9 +47,7 @@ class Order(models.Model):
 	product = models.ForeignKey(Product, on_delete= models.SET_NULL, null=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
-	note = models.CharField(max_length=200, null=True)
-
-	tags = models.ManyToManyField(Tag)
+	note = models.CharField(max_length=200, null=True, blank=True)
 
 	def __str__(self):
 		return str(self.product)
