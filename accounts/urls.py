791c7c76bd22781d318ba8d2ed500ac5f0161a55
maamirmughal <101181756+mc514@users.noreply.github.com>
2025-02-22 23:01:33 +0400

    CURD

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,15 @@ from accounts import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('customer/',views.customer, name='customer'),
+    #---------------------------------Customer URLS---------------------------------------
+    path('customers/',views.customer_list, name='customer_list'),
+    path('customer/<str:pk>/',views.customer, name='customer'),
+    # path('customers_detail/<str:pk>/',views.customers, name='customers'),
+
     path('products/',views.products, name='products'),
+    path('create_order/<str:pk>/',views.createOrder, name='create_order'),
+    path('update_order/<str:pk>',views.updateOrder, name='update_order'),
+    path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
+
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import *
 
 # Create your views here.
 
@@ -20,10 +21,69 @@ def home(request):
                'delivered': delivered, 'pending': pending}
     return render(request, 'accounts/dashboard.html', context)
 
-def customer(request):
+#---------------------------------Customer Views---------------------------------------
+
+def customer(request, pk):
     # return HttpResponse("Customer Page")
-     return render(request, 'accounts/customer.html')
+    customer = Customer.objects.get(id=pk)
+
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    context = {'customer': customer, 'orders': orders, 'order_count': order_count}
+    return render(request, 'accounts/customer.html',context)
+
+def customer_list(request):
+    # return HttpResponse("Customer Page")
+    customers = Customer.objects.all()
+    return render(request, 'accounts/customer_list.html',{'customers':customers})
+
+#---------------------------------Product Views---------------------------------------
+
 def products(request):
     # return HttpResponse("Product Page")
     products = Product.objects.all()
     return render(request, 'accounts/products.html',{'products': products})
+
+#-------------------(CREATE ORDER VIEWS) -------------------
+
+def createOrder(request,pk):
+	action = 'create'
+	customer = Customer.objects.get(id=pk)
+	form = OrderForm(initial={'customer': customer})
+	if request.method == 'POST':
+        # print('Printng POST:',request.POST)
+		form = OrderForm(request.POST)
+	
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context =  {'form':form}
+	return render(request, 'accounts/order_form.html', context)
+
+#-------------------(UPDATE ORDER VIEWS) -------------------
+
+def updateOrder(request,pk):
+	action = 'update'
+	order = Order.objects.get(id=pk)
+	form = OrderForm(instance=order)
+	if request.method == 'POST':
+    # print('Printng POST:',request.POST)
+		form = OrderForm(request.POST, instance=order)
+	
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context =  {'form':form}
+	return render(request, 'accounts/order_form.html', context)
+
+#-------------------(DELETE VIEWS) -------------------
+def deleteOrder(request, pk):
+	order = Order.objects.get(id=pk)
+	if request.method == 'POST':
+		order.delete()
+		return redirect('/')
+		
+	return render(request, 'accounts/delete_item.html', {'item':order})
